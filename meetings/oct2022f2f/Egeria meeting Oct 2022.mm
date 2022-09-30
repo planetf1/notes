58b8ac8f3fbf3466582a578f6fe927f894e26a33
Nigel Jones <nigel.l.jones+git@gmail.com>
2022-09-30 13:18:10 +0100

    Add doc on PR reviews
    Signed-off-by: Nigel Jones <nigel.l.jones+git@gmail.com>

---
+++ meetings/oct2022f2f/Egeria meeting Oct 2022.mm
@@ -0,0 +1,154 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Egeria meeting Oct 2022" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1664537864008" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm" followedTemplateLocation="template:/standard-1.6.mm" followedMapLastTime="1662825478000"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Agenda Items" POSITION="right" ID="ID_309330472" CREATED="1664537872523" MODIFIED="1664537875441">
+<edge COLOR="#ff0000"/>
+<node TEXT="" ID="ID_262765978" CREATED="1664537919279" MODIFIED="1664537919279"/>
+<node TEXT="Manual metadata creation in egeria" ID="ID_416746915" CREATED="1664537922383" MODIFIED="1664537928817"/>
+<node TEXT="platform security/access control for http reset apis" ID="ID_484084982" CREATED="1664537930611" MODIFIED="1664537946355"/>
+<node TEXT="JDBC connector" ID="ID_105834218" CREATED="1664537948566" MODIFIED="1664537951281"/>
+<node TEXT="UI" ID="ID_1806876695" CREATED="1664537956518" MODIFIED="1664537959793"/>
+<node TEXT="Indexed search" ID="ID_834612599" CREATED="1664537963231" MODIFIED="1664537966473"/>
+<node TEXT="Dojos" ID="ID_557888205" CREATED="1664537971815" MODIFIED="1664537973843">
+<node TEXT="TODO: Options for making easier?" ID="ID_270780285" CREATED="1664538555764" MODIFIED="1664538561355"/>
+</node>
+<node TEXT="Adoption" ID="ID_1427060631" CREATED="1664537974679" MODIFIED="1664537976282"/>
+<node TEXT="Conferences" ID="ID_1903077079" CREATED="1664537976646" MODIFIED="1664537978274"/>
+<node TEXT="Next workshops" ID="ID_17181423" CREATED="1664537979011" MODIFIED="1664537981095"/>
+<node TEXT="TAC review for Egeria" ID="ID_569377052" CREATED="1664537981566" MODIFIED="1664537984611">
+<node TEXT="" ID="ID_1179378509" CREATED="1664537991618" MODIFIED="1664537991618">
+<node TEXT="" ID="ID_1864403911" CREATED="1664537989569" MODIFIED="1664537989569"/>
+</node>
+</node>
+<node TEXT="Certificate Management" ID="ID_463914959" CREATED="1664537993935" MODIFIED="1664537998871">
+<node TEXT="TODO: Summarise work" ID="ID_1405037298" CREATED="1664538536606" MODIFIED="1664538547564"/>
+</node>
+<node TEXT="Python Client" ID="ID_757367139" CREATED="1664537999653" MODIFIED="1664538002572"/>
+<node TEXT="Support for RPC" ID="ID_1303238857" CREATED="1664538002987" MODIFIED="1664538006166"/>
+<node TEXT="Release 4" ID="ID_1533944207" CREATED="1664538022070" MODIFIED="1664538024521">
+<node TEXT="TODO: summarise big items" ID="ID_364925378" CREATED="1664538329958" MODIFIED="1664538335305">
+<node TEXT="gradle" ID="ID_921867578" CREATED="1664538337983" MODIFIED="1664538355664"/>
+</node>
+</node>
+<node TEXT="Repositories and Artifacts" ID="ID_268078099" CREATED="1664538028671" MODIFIED="1664538033144">
+<node TEXT="TODO: summarise repos" ID="ID_1178878767" CREATED="1664538324527" MODIFIED="1664538329106"/>
+</node>
+<node TEXT="Improving code quality" ID="ID_533424460" CREATED="1664538033601" MODIFIED="1664538038040">
+<node TEXT="TODO: Summarise reports" ID="ID_228371219" CREATED="1664538318894" MODIFIED="1664538323257"/>
+<node TEXT="TODO: work to do ie SBOM" ID="ID_1829740201" CREATED="1664538578413" MODIFIED="1664538584298"/>
+</node>
+<node TEXT="release workload" ID="ID_968584645" CREATED="1664538038647" MODIFIED="1664538041143">
+<node TEXT="TODO: Summarise tasks" ID="ID_1994591331" CREATED="1664538313799" MODIFIED="1664538317936">
+<node TEXT="dependency updates" ID="ID_683110915" CREATED="1664538413718" MODIFIED="1664538416327"/>
+<node TEXT="branch/build/release" ID="ID_336939820" CREATED="1664538417122" MODIFIED="1664538421840"/>
+<node TEXT="LF integration with signing" ID="ID_758433729" CREATED="1664538423167" MODIFIED="1664538429577"/>
+</node>
+<node TEXT="Release frequency" ID="ID_1399783629" CREATED="1664538382216" MODIFIED="1664538386266">
+<node TEXT="Less often" ID="ID_94916899" CREATED="1664538386694" MODIFIED="1664538396175">
+<node TEXT="more to talk about" ID="ID_1083087043" CREATED="1664538397388" MODIFIED="1664538400890"/>
+<node TEXT="less disruptive" ID="ID_1738250333" CREATED="1664538401299" MODIFIED="1664538403759"/>
+<node TEXT="XX work can build up" ID="ID_889791591" CREATED="1664538404655" MODIFIED="1664538408514"/>
+</node>
+</node>
+</node>
+<node TEXT="kubernetes deployments" ID="ID_434748266" CREATED="1664538041798" MODIFIED="1664538045260">
+<node TEXT="TODO: Summarise where we are" ID="ID_1279098917" CREATED="1664538306894" MODIFIED="1664538312552"/>
+</node>
+<node TEXT="production requirements in general" ID="ID_245313684" CREATED="1664538049752" MODIFIED="1664538056858"/>
+<node TEXT="enforcing reviews for PRs" ID="ID_1841185034" CREATED="1664538057185" MODIFIED="1664538061115">
+<node TEXT="TODO: Can we find stats/rationale?" ID="ID_1915180812" CREATED="1664538275485" MODIFIED="1664538284104"/>
+<node TEXT="TODO: process plugin" ID="ID_1985485845" CREATED="1664538285308" MODIFIED="1664538295897"/>
+</node>
+</node>
+<node TEXT="Themes of outstanding work/concerns" POSITION="left" ID="ID_271210353" CREATED="1664538071933" MODIFIED="1664538160640">
+<edge COLOR="#0000ff"/>
+<node TEXT="Releases" ID="ID_524700304" CREATED="1664538093253" MODIFIED="1664538095257">
+<node TEXT="How frequent" ID="ID_1378383581" CREATED="1664538198566" MODIFIED="1664538201514"/>
+<node TEXT="repo owner responsibilities" ID="ID_846234852" CREATED="1664538202486" MODIFIED="1664538207324"/>
+<node TEXT="common scripts/process" ID="ID_537966095" CREATED="1664538207906" MODIFIED="1664538212499"/>
+</node>
+<node TEXT="repo management" ID="ID_528468564" CREATED="1664538095781" MODIFIED="1664538098341"/>
+<node TEXT="security scans" ID="ID_130469338" CREATED="1664538099166" MODIFIED="1664538101642"/>
+<node TEXT="code scans" ID="ID_569116012" CREATED="1664538101845" MODIFIED="1664538103562"/>
+<node TEXT="security/SBOM" ID="ID_352513005" CREATED="1664538107702" MODIFIED="1664538111025"/>
+<node TEXT="tutorials/labs" ID="ID_1698242662" CREATED="1664538111710" MODIFIED="1664538115442"/>
+<node TEXT="k8s operator" ID="ID_1379918507" CREATED="1664538116004" MODIFIED="1664538118390"/>
+<node TEXT="Atlas connector" ID="ID_54831930" CREATED="1664538122894" MODIFIED="1664538126056"/>
+<node TEXT="Issue management" ID="ID_1165108760" CREATED="1664538162552" MODIFIED="1664538166066"/>
+<node TEXT="Testing - code coverage" ID="ID_1558001277" CREATED="1664538168971" MODIFIED="1664538175233"/>
+<node TEXT="maintainer responsibilities" ID="ID_1429705494" CREATED="1664538248262" MODIFIED="1664538253928"/>
+<node TEXT="Advocacy" ID="ID_1804850758" CREATED="1664538356750" MODIFIED="1664538359648"/>
+<node TEXT="Blogs, videos, education" ID="ID_559676522" CREATED="1664538360278" MODIFIED="1664538364433">
+<node TEXT="Small snippets" ID="ID_103548141" CREATED="1664538364708" MODIFIED="1664538367920"/>
+</node>
+</node>
+</node>
+</map>
---
+++ meetings/oct2022f2f/PRreviews.md
@@ -0,0 +1,26 @@
+# Enforcing PR Reviews
+
+## Rationale
+
+* Improve code quality
+* Demonstrate code governance
+
+## Implications
+
+* Slower to get code changes in
+* Requires other maintainers to check for open PRs and act upon them
+
+## Mechanisms
+
+* defined by branch
+* required approvals: n (1..)
+* Require review from code owner
+* Optional: Limit approvals/require changes only to contributors/maintainers
+
+### Bots
+* Auto approve (not merge) after no other reviews added?
+* label requires reviews -- label defines how many
+
+## Other thoughts
+* Slack bot?
+* Action to bypass checks based on label ('urgent') etc
